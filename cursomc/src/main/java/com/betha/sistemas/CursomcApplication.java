467e9d24eaaf92f1339566e84f99a5490a0bb382
Kayo <arimanoelgomes@gmail.com>
2023-03-06 23:20:43 -0300

    Criando operacao de instanciacao

--- cursomc/src/main/java/com/betha/sistemas/CursomcApplication.java
+++ cursomc/src/main/java/com/betha/sistemas/CursomcApplication.java
@@ -1,14 +1,33 @@
 package com.betha.sistemas;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.betha.sistemas.domain.Categoria;
+import com.betha.sistemas.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
